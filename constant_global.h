8366adc1ecf09ebb1e5daa9c4fde3c48b5d4bcc3
TitouanDor <titouan.dorier@outlook.fr>
2025-10-25 19:42:47 +0200

    add player structur and some function associate with it

---
+++ constant_global.h
@@ -0,0 +1,6 @@
+#ifndef CONSTANT_GLOBAL_H
+#define CONSTANT_GLOBAL_H
+
+#define SPEED 1
+
+#endif
--- main.c
+++ main.c
@@ -2,11 +2,16 @@
 #include <stdio.h>
 #include <SDL3/SDL.h>
 #include <SDL3/SDL_main.h>
+#include "Player.h"
+#include "constant_global.h"
+
 
 
 /* We will use this renderer to draw into this window every frame. */
 static SDL_Window *window = NULL;
 static SDL_Renderer *renderer = NULL;
+static Player* p = NULL;
+
 
 /* This function runs once at startup. */
 SDL_AppResult SDL_AppInit(void **appstate, int argc, char *argv[])
@@ -24,15 +29,18 @@ SDL_AppResult SDL_AppInit(void **appstate, int argc, char *argv[])
     }
     SDL_SetRenderLogicalPresentation(renderer, 640, 480, SDL_LOGICAL_PRESENTATION_LETTERBOX);
 
+    p = InitPlayer();
+
     return SDL_APP_CONTINUE;  /* carry on with the program! */
 }
 
 /* This function runs when a new event (mouse input, keypresses, etc) occurs. */
 SDL_AppResult SDL_AppEvent(void *appstate, SDL_Event *event)
 {
-    if (event->type == SDL_EVENT_QUIT) {
+    if(event->type == SDL_EVENT_QUIT){
         return SDL_APP_SUCCESS;  /* end the program, reporting success to the OS. */
     }
+
     return SDL_APP_CONTINUE;  /* carry on with the program! */
 }
 
@@ -40,15 +48,17 @@ SDL_AppResult SDL_AppEvent(void *appstate, SDL_Event *event)
 SDL_AppResult SDL_AppIterate(void *appstate)
 {
     const double now = ((double)SDL_GetTicks()) / 1000.0;  /*convert from milliseconds to seconds. */
+    const bool* state = SDL_GetKeyboardState(NULL);
+    p = MovePlayer(p, state);
+
     SDL_SetRenderDrawColor(renderer, 155, 155, 155, SDL_ALPHA_OPAQUE); /*Grey, full alpha*/
     /* clear the window to the draw color. */
     SDL_RenderClear(renderer);
 
-    
-
-
-
-
+    /*Render player*/
+    SDL_SetRenderDrawColor(renderer, 255, 0, 0, SDL_ALPHA_OPAQUE);  /* red, full alpha */
+    SDL_RenderRect(renderer, &p->skin);
+    SDL_RenderFillRect(renderer, &p->skin);
 
     /* put the newly-cleared rendering on the screen. */
     SDL_RenderPresent(renderer);
