4ac8a9553522172ecc390d63b5071166c873b5bd
TitouanDor <titouan.dorier@outlook.fr>
2025-10-26 11:47:57 +0100

    add struct Gamectx

--- main.c
+++ main.c
@@ -4,35 +4,47 @@
 #include <SDL3/SDL_main.h>
 #include "Player.h"
 #include "constant_global.h"
+#include "Element.h"
 
 
 
 /* We will use this renderer to draw into this window every frame. */
-static SDL_Window *window = NULL;
-static SDL_Renderer *renderer = NULL;
 static Player* p = NULL;
 
+typedef struct{
+    SDL_Window *window;
+    SDL_Renderer *renderer;
+    int canva;
+}Gamectx;
 
 /* This function runs once at startup. */
-SDL_AppResult SDL_AppInit(void **appstate, int argc, char *argv[])
+SDL_AppResult SDL_AppInit(void** appstate, int argc, char* argv[])
 {
     if (!SDL_Init(SDL_INIT_VIDEO)) {
         SDL_Log("Couldn't initialize SDL: %s", SDL_GetError());
         return SDL_APP_FAILURE;
     }
 
-    if (!SDL_CreateWindowAndRenderer("Adventur", WINDOW_WIDTH, WINDOW_HEIGHT, SDL_WINDOW_RESIZABLE, &window, &renderer)) {
+    Gamectx* gameState = (Gamectx*)SDL_calloc(1, sizeof(Gamectx));
+    if (!gameState) {
+        return SDL_APP_FAILURE;
+    }
+    *appstate = gameState;
+
+    if (!SDL_CreateWindowAndRenderer("Adventur", WINDOW_WIDTH, WINDOW_HEIGHT, SDL_WINDOW_RESIZABLE, &gameState->window, &gameState->renderer)) {
         SDL_Log("Couldn't create window/renderer: %s", SDL_GetError());
         return SDL_APP_FAILURE;
     }
 
     p = InitPlayer();
 
+    
+
     return SDL_APP_CONTINUE;  /* carry on with the program! */
 }
 
 /* This function runs when a new event (mouse input, keypresses, etc) occurs. */
-SDL_AppResult SDL_AppEvent(void *appstate, SDL_Event *event)
+SDL_AppResult SDL_AppEvent(void* appstate, SDL_Event* event)
 {
     if(event->type == SDL_EVENT_QUIT){
         return SDL_APP_SUCCESS;  /* end the program, reporting success to the OS. */
@@ -42,30 +54,37 @@ SDL_AppResult SDL_AppEvent(void *appstate, SDL_Event *event)
 }
 
 /* This function runs once per frame, and is the heart of the program. */
-SDL_AppResult SDL_AppIterate(void *appstate)
+SDL_AppResult SDL_AppIterate(void* appstate)
 {
+    Gamectx* gameState = (Gamectx *)appstate;
     const double now = ((double)SDL_GetTicks()) / 1000.0;  /*convert from milliseconds to seconds. */
     const bool* state = SDL_GetKeyboardState(NULL);
     p = MovePlayer(p, state);
 
-    SDL_SetRenderDrawColor(renderer, 155, 155, 155, SDL_ALPHA_OPAQUE); /*Grey, full alpha*/
+    SDL_SetRenderDrawColor(gameState->renderer, 155, 155, 155, SDL_ALPHA_OPAQUE); /*Grey, full alpha*/
     /* clear the window to the draw color. */
-    SDL_RenderClear(renderer);
+    SDL_RenderClear(gameState->renderer);
 
     /*Render player*/
-    SDL_SetRenderDrawColor(renderer, 255, 0, 0, SDL_ALPHA_OPAQUE);  /* red, full alpha */
-    SDL_RenderRect(renderer, &p->skin);
-    SDL_RenderFillRect(renderer, &p->skin);
+    SDL_SetRenderDrawColor(gameState->renderer, 255, 0, 0, SDL_ALPHA_OPAQUE);  /* red, full alpha */
+    SDL_RenderRect(gameState->renderer, &p->skin);
+    SDL_RenderFillRect(gameState->renderer, &p->skin);
 
     /* put the newly-cleared rendering on the screen. */
-    SDL_RenderPresent(renderer);
+    SDL_RenderPresent(gameState->renderer);
 
     return SDL_APP_CONTINUE;  /* carry on with the program! */
 }
 
 /* This function runs once at shutdown. */
-void SDL_AppQuit(void *appstate, SDL_AppResult result)
+void SDL_AppQuit(void* appstate, SDL_AppResult result)
 {
+    if (appstate != NULL) {
+        Gamectx* as = (Gamectx*)appstate;
+        SDL_DestroyRenderer(as->renderer);
+        SDL_DestroyWindow(as->window);
+        SDL_free(as);
+    }
     /* SDL will clean up the window/renderer for us. */
     printf("Fin du programme\n");
 }
