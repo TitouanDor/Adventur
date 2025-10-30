5556aace216b9cb194afe3ad0cc6e55645cc568e
TitouanDor <titouan.dorier@outlook.fr>
2025-10-30 13:17:15 +0100

    add InitLog

--- Log.c
+++ Log.c
@@ -4,6 +4,15 @@
 #include <stdlib.h>
 #include <time.h>
 
+int InitLog(void){
+    FILE *fichier = fopen("Log.txt", "w");
+    if (fichier == NULL) {
+        printf("Erreur d'ouverture du fichier.\n");
+        return 1;
+    }
+    return 0;
+}
+
 int WriteLog(char *texte) {
     FILE *fichier = fopen("Log.txt", "a");
     if (fichier == NULL) {
@@ -29,6 +38,7 @@ int Import_canva(Canva* canva){
     }
     fprintf(fichier, "|   id : %d\n", canva->id);
     fprintf(fichier,"|   nb_wall : %d\n", canva->nb_wall);
+    fprintf(fichier,"|   nb_gate : %d\n", canva->nb_gate);
     fprintf(fichier,"|   id_next_canva :\n");
     for(int i = 0; i < 4; i++) {
         fprintf(fichier,"|   |   [%d] : %d\n", i, canva->id_next_canva[i]);
--- Log.h
+++ Log.h
@@ -2,6 +2,7 @@
 #define LOG_H
 #include "canva.h"
 
+int InitLog(void);
 int WriteLog(char *texte);
 int Import_canva(Canva* canva);
 
